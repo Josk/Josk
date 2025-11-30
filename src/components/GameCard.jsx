1539fe1a8cfb1ab8103867f6018ebcbf0a95c217
Josk <romain.pedra@gmail.com>
2025-11-30 20:24:49 +0100

    Updated youtube regex to work with shorts

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -19,7 +19,7 @@ const GameCard = ({ game }) => {
 
     const getYoutubeVideoId = (url) => {
         if (!url) return null
-        const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/
+        const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=|shorts\/)([^#&?]*).*/
         const match = url.match(regExp)
         return (match && match[2].length === 11) ? match[2] : null
     }
